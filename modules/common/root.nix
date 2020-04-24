925066cdf8371af82a6ec7869046a62c4a355ac2
Dustin Frisch <fooker@lab.sh>
2020-04-24 16:06:01 +0200

    Uhrm

--- modules/common/root.nix
+++ modules/common/root.nix
@@ -10,7 +10,7 @@ with lib;
   };
 
   config = mkIf config.common.root.enable {
-    users.users.root = {
+    users.users."root" = {
       hashedPassword = "$6$3S/rgJ8.Dz7ak$UeqTgpMfIVAZK3.82QgisbZFmyhPE1f9JNEACx8agIJkNIPECNY5cXaqCiTFxo0PRM/Jhch/qjVLlpCH1C/Lr.";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2nkarN0+uSuP5sGwDCb9KRu+FCjO/+da4VypGanPUZ fooker@k-2so"
