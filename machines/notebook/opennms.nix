52ec6e84187618e62325c322d459673536887b91
Dustin Frisch <fooker@lab.sh>
2020-08-10 13:00:26 +0200

    Added openvpn client to opennms

--- machines/notebook/opennms.nix
+++ machines/notebook/opennms.nix
@@ -1,10 +1,24 @@
 { pkgs, sources, ... }:
 
 {
+  # Hack required to get running nodejs downloaded by maven (who even things this is a god idea?)
   system.activationScripts.ldso = pkgs.lib.stringAfter [ "usrbinenv" ] ''                       
     mkdir -m 0755 -p /lib64                                                                
     ln -sfn ${pkgs.glibc.out}/lib64/ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2.tmp   
     mv -f /lib64/ld-linux-x86-64.so.2.tmp /lib64/ld-linux-x86-64.so.2 # atomically replace 
   '';                                                                                      
 
+  services.openvpn.servers.opennms = {
+    autoStart = false;
+    config = "config /etc/openvpn/opennms.ovpn";
+  };
+
+  deployment.secrets = {
+      "openvpn-opennms" = {
+        source = toString ./secrets/opennms.ovpn;
+        destination = "/etc/openvpn/opennms.ovpn";
+        owner.user = "root";
+        owner.group = "root";
+      };
+    };
 }
