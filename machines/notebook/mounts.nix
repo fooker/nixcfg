2d63350aa9c0368b551358ac17968e95b9e5fd67
Dustin Frisch <fooker@lab.sh>
2020-09-23 13:28:18 +0200

    notebook: Added mount for cantina

--- machines/notebook/mounts.nix
+++ machines/notebook/mounts.nix
@@ -1,9 +1,20 @@
 { pkgs, sources, ... }:
 
-{
+let
+  secrets = import ./secrets.nix;
+in {
   fileSystems."/mnt/vault" = {
     device = "nas.home.open-desk.net:/";
     fsType = "nfs4";
     options = ["x-systemd.automount" "noauto"];
+  };
+  
+  fileSystems."/mnt/cantina" = {
+    device = "//192.168.31.16/cantina";
+    fsType = "cifs";
+    options = ["x-systemd.automount" "noauto"
+      "username=${secrets.mounts.cantina.password}"
+      "password=${secrets.mounts.cantina.password}"
+    ];
   };
 }
