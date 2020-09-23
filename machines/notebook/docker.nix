441e07eb656dd284460cf008ffba2161d224626d
Dustin Frisch <fooker@lab.sh>
2020-09-23 13:27:07 +0200

    Prune docker automatically

--- machines/notebook/docker.nix
+++ machines/notebook/docker.nix
@@ -4,6 +4,13 @@
   virtualisation.docker = {
     enable = true;
     storageDriver = "btrfs";
+
+    extraOptions = "--debug";
+
+    autoPrune = {
+      enable = true;
+      flags = [ "--all" ];
+    };
   };
 
   environment.systemPackages = with pkgs; [
