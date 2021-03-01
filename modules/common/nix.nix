ebbc75473bc8f5d885d18ca25fcb1f0e6e03f5cb
Dustin Frisch <fooker@lab.sh>
2021-03-01 10:51:25 +0100

    Less nix GC

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -13,14 +13,14 @@ with lib;
     # Take out the trash
     nix.gc = {
       automatic = true;
-      dates = "weekly";
+      dates = "monthly";
       options = "--delete-older-than 30d";
     };
 
     # Optimize the store
     nix.optimise = {
       automatic = true;
-      dates = [ "weekly" ];
+      dates = [ "monthly" ];
     };
 
     # Who cares about licenses?
