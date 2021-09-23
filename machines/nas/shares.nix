7fcff2246bbaa7e1495ee75848ac64a948b7055e
Dustin Frisch <fooker@lab.sh>
2021-09-23 22:09:00 +0200

    machines/nas: fix share firewall

--- machines/nas/shares.nix
+++ machines/nas/shares.nix
@@ -58,15 +58,15 @@
   firewall.rules = dag: with dag; {
     inet.filter.input = {
       nfs = between [ "established" ] [ "drop" ] [
-        ''ip saddr 172.23.200.0/25 tcp dport 2049 accept''
-        ''ip saddr 172.23.200.0/25 udp dport 2049 accept''
+        ''ip saddr 172.23.200.128/25 tcp dport 2049 accept''
+        ''ip saddr 172.23.200.128/25 udp dport 2049 accept''
         ''ip6 saddr fd79:300d:6056:100::/64 tcp dport 2049 accept''
         ''ip6 saddr fd79:300d:6056:100::/64 udp dport 2049 accept''
       ];
 
       smb = between [ "established" ] [ "drop" ] [
-        ''ip saddr 172.23.200.0/25 tcp dport { 137, 138, 139, 445 } accept''
-        ''ip saddr 172.23.200.0/25 udp dport { 137, 138, 139, 445 } accept''
+        ''ip saddr 172.23.200.128/25 tcp dport { 137, 138, 139, 445 } accept''
+        ''ip saddr 172.23.200.128/25 udp dport { 137, 138, 139, 445 } accept''
         ''ip6 saddr fd79:300d:6056:100::/64 tcp dport { 137, 138, 139, 445 } accept''
         ''ip6 saddr fd79:300d:6056:100::/64 udp dport { 137, 138, 139, 445 } accept''
       ];
