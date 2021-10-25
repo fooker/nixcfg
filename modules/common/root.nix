44368fcfb0c28f69ed5845f54d6b630e608cdf6e
Dustin Frisch <fooker@lab.sh>
2021-10-25 09:39:55 +0200

    all: use zsh for root

--- modules/common/root.nix
+++ modules/common/root.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 
@@ -15,11 +15,18 @@ in
 
   config = mkIf config.common.root.enable {
     users.users."root" = {
+      shell = pkgs.zsh;
+
       inherit (secrets.users.root) hashedPassword;
 
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2nkarN0+uSuP5sGwDCb9KRu+FCjO/+da4VypGanPUZ fooker@k-2so"
       ];
+    };
+
+    programs.zsh = {
+      enable = true;
+      histSize = 10000;
     };
   };
 }
