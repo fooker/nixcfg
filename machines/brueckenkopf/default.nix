8f3bde015f047882cbdcb0c422c97a16b6b70b57
Dustin Frisch <fooker@lab.sh>
2022-05-15 00:23:18 +0200

    machines/brueckenkopf: connect to cantina

--- machines/brueckenkopf/default.nix
+++ machines/brueckenkopf/default.nix
@@ -16,4 +16,15 @@ in
   backup.passphrase = secrets.backup.passphrase;
 
   dns.host.interface = "ext";
+
+  fileSystems."/mnt/cantina" = {
+    device = "//192.168.31.16/cantina";
+    fsType = "cifs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "username=${secrets.mounts.cantina.username}"
+      "password=${secrets.mounts.cantina.password}"
+    ];
+  };
 }
