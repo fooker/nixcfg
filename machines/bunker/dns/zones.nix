d25edc1eb0e837a0d59daa8ea2dea5f1b554fe30
Dustin Frisch <fooker@lab.sh>
2023-01-06 13:32:33 +0100

    machines/bunker: add DS records for dyn subdomain delegation

--- machines/bunker/dns/zones.nix
+++ machines/bunker/dns/zones.nix
@@ -59,6 +59,21 @@ with lib;
 
           parent = {
             NS = nameservers;
+
+            DS = [
+              {
+                keyTag = 58674;
+                algorithm = "ED25519";
+                digestType = "SHA-256";
+                digest = "d9714dcee6b66460e4bbab62201728d5b488b94d423b4b31c9bc71724176c992";
+              }
+              {
+                keyTag = 58674;
+                algorithm = "ED25519";
+                digestType = "SHA-384";
+                digest = "8c27502f7f35d7c2e20523776f6a29bbc59b29633786b3f406061e5dc24de290f5577ba575248b7d22451d14d8a29b66";
+              }
+            ];
           };
         };
 
