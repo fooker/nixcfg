ee1efa9b1c3f64d9ceec215eb611dfda0b07dcea
Dustin Frisch <fooker@lab.sh>
2023-09-06 11:49:34 +0200

    machines/notebook: add c3radio script

--- machines/notebook/home/scripts.nix
+++ machines/notebook/home/scripts.nix
@@ -1,15 +1,21 @@
 { pkgs, ... }:
 
 let
-  weechat-connect = pkgs.writeScriptBin "weechat-connect" (with pkgs; ''
-    #!${stdenv.shell}
-
-    exec ${mosh}/bin/mosh "weechat@weechat.open-desk.net" -- \
+  weechat-connect = pkgs.writeScriptBin "weechat-connect" ''
+    exec ${pkgs.mosh}/bin/mosh "weechat@weechat.open-desk.net" -- \
       tmux -S /var/lib/weechat/tmux.session attach-session -t weechat
-  '');
+  '';
+
+  c3radio = pkgs.writeScriptBin "c3radio" ''
+    selected=$(ls -d -1 /mnt/vault/downloads/c3sets/by-id/* | shuf | head -n 1)
+    echo "Playing $selected"
+
+    ${pkgs.mpv}/bin/mpv "$selected"
+  '';
 in
 {
   home.packages = [
     weechat-connect
+    c3radio
   ];
 }
