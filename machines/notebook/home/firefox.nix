ecc35b64807f6f9fd6369a26b9bb3bd19fc783b2
Dustin Frisch <fooker@lab.sh>
2025-03-04 13:52:31 +0100

    machines/notebook: switch from firefox to librewolf

--- machines/notebook/home/firefox.nix
+++ machines/notebook/home/firefox.nix
@@ -1,8 +1,30 @@
 { pkgs, ... }:
 
 {
-  programs.firefox = {
+  programs.librewolf = {
     enable = true;
     package = pkgs.librewolf;
+
+    settings = {
+      "webgl.disabled" = false;
+
+      "cookiebanners.service.mode.privateBrowsing" = 2;
+      "cookiebanners.service.mode" = 2;
+
+      "privacy.donottrackheader.enabled" = true;
+
+      "privacy.fingerprintingProtection" = true;
+      "privacy.resistFingerprinting" = true;
+
+      "privacy.trackingprotection.emailtracking.enabled" = true;
+      "privacy.trackingprotection.enabled" = true;
+      "privacy.trackingprotection.fingerprinting.enabled" = true;
+      "privacy.trackingprotection.socialtracking.enabled" = true;
+
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.cookies" = false;
+
+      "network.cookie.lifetimePolicy" = 0;
+    };
   };
 }
