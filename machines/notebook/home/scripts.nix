d505680db2b60491dd808b0ea19b3255c4bc1986
Dustin Frisch <fooker@lab.sh>
2025-04-09 17:52:57 +0200

    machines/notebook: add telefonliste script

--- machines/notebook/home/scripts.nix
+++ machines/notebook/home/scripts.nix
@@ -22,11 +22,34 @@ let
       exec "$SHELL"
     )
   '';
+
+  telefonliste =
+    let
+      script = pkgs.fetchurl {
+        url = "https://gist.githubusercontent.com/fooker/db6948cb3c3551e5516dd6c875f6de05/raw/6dd568aa3494712c94f6f7a2976fc1248541fc8e/telefonliste.py";
+        hash = "sha256-zL32S+pdOPpkHGetaKMYhL4OxLiryQt4JoyUwqn8WJE=";
+      };
+    in
+    pkgs.writers.writePython3Bin "telefonliste"
+      {
+        flakeIgnore = [ "E265" ];
+        libraries = with pkgs.python3Packages; [
+          requests
+          pandas
+          openpyxl
+          click
+          unidecode
+          thefuzz
+        ];
+      }
+      script;
+
 in
 {
   home.packages = [
     weechat-connect
     c3radio
     tmpsh
+    telefonliste
   ];
 }
