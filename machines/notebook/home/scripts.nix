889a0f4d45c63bdf1ec5b30a6bf94932dfd05c61
Dustin Frisch <fooker@lab.sh>
2025-05-19 16:34:53 +0200

    machines/notebook: Update telefonliste script

--- machines/notebook/home/scripts.nix
+++ machines/notebook/home/scripts.nix
@@ -26,8 +26,8 @@ let
   telefonliste =
     let
       script = pkgs.fetchurl {
-        url = "https://gist.githubusercontent.com/fooker/db6948cb3c3551e5516dd6c875f6de05/raw/6dd568aa3494712c94f6f7a2976fc1248541fc8e/telefonliste.py";
-        hash = "sha256-zL32S+pdOPpkHGetaKMYhL4OxLiryQt4JoyUwqn8WJE=";
+        url = "https://gist.githubusercontent.com/fooker/db6948cb3c3551e5516dd6c875f6de05/raw/14e779c692ab928f7e652d2cfc0175707daedf73/telefonliste.py";
+        hash = "sha256-qaoADs77gNBfo5/TLf8n+bDlezSaat3/2h7mxSQHrnI=";
       };
     in
     pkgs.writers.writePython3Bin "telefonliste"
