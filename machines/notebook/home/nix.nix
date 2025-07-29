fedd14f387756b8c1f0c1b3d7429045d49f64061
Dustin Frisch <fooker@lab.sh>
2025-07-29 23:14:22 +0200

    machines/notebook/home: add work caches to nix

--- machines/notebook/home/nix.nix
+++ machines/notebook/home/nix.nix
@@ -8,6 +8,12 @@
   nix.extraOptions = ''
     builders-use-substitutes = true
     !include ${config.sops.secrets."nix/access-tokens".path}
+
+    extra-substituters = https://colmena.cachix.org
+    extra-substituters = https://cache.nix.hlsb.hs-fulda.de/hlsb-nixcfg
+
+    extra-trusted-public-keys = colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg=
+    extra-trusted-public-keys = hlsb-nixcfg:JCWYhhI49R/+dvIeeBpq3EPdRQ5mGQWp9YBxIV+LYB0=
   '';
 
   sops.secrets."nix/access-tokens" = {
